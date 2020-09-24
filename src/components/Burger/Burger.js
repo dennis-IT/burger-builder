1fad2630e029d9166bcbeff4e423e43fd360c690
dennis-IT <dennis.huy.ta@gmail.com>
2020-09-24 16:05:27 -0400

    update dynamic ingredients

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import classes from './Burger.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = props => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])] //!NOTE: this will create an array with length = props.ingredients[igKey]
+                .map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />
+                });
+        });
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {/* <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" /> */}
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burger;
